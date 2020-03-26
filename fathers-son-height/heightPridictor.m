bea98fa82660af4ea09c4934a7743722ff5e7903
OrionMat <36226295+OrionMat@users.noreply.github.com>
2020-03-26 16:57:06 -0400

    Added Feature Scaling

--- fathers-son-height/heightPridictor.m
+++ fathers-son-height/heightPridictor.m
@@ -19,20 +19,29 @@ costNorm = costMSE(X, thetaNorm, y, m)
 thetaGrad = [0 ; 0];
 alpha = 0.0001;
 itterations = 25;
-% feature scaling
-scale = (x-mean(x))./std(x);
-Xscale = [ones(m, 1) scale]; 
-
 costGrad0 = costMSE(X, thetaGrad, y, m)
 [thetaGrad, Jpast] = gradDescent(X, y, thetaGrad, alpha, m, itterations);
 costGrad = costMSE(X, thetaGrad, y, m)
 
+% feature scaling and gradient descent
+scale = (x-mean(x))./std(x);
+Xscale = [ones(m, 1) scale]; 
+thetaGradScale = [0 ; 0];
+alphaScale = 1;
+costGradScale0 = costMSE(Xscale, thetaGradScale, y, m)
+[thetaGradScale, JpastScale] = gradDescent(Xscale, y, thetaGradScale, alphaScale, m, itterations);
+costGradSale = costMSE(Xscale, thetaGradScale, y, m)
+ 
 
 % plotting 
 
 theta0 = linspace(-20, 80, 100)';
-theta1 = linspace(-3, 3, 100)';
+theta1 = linspace(-15, 15, 100)';
 J_vals = costValues(X, y, theta0, theta1, m);
+
+theta0scale = linspace(-5, 140, 100)';
+theta1scale = linspace(-100, 100, 100)';
+J_valScale = costValues(Xscale, y, theta0scale, theta1scale, m);
 
 figure 1;
 % figure 1 subplot 1: raw data
@@ -44,7 +53,7 @@ title('Father-Son Heights');
 
 % figure 1 subplot 2: cost function
 subplot(2,2,2);
-surf(theta0, theta1, J_vals)
+surf(theta0, theta1, J_vals);
 xlabel('\theta_0'); 
 ylabel('\theta_1');
 zlabel('Cost');
@@ -73,27 +82,28 @@ ylabel('\theta_1');
 title('MSE Cost Contours');
 
 figure 2;
-% figure 2 subplot 1: cost function
+% figure 2 subplot 1: scaled cost function
 subplot(2,2,1);
-surf(theta0, theta1, J_vals)
-xlabel('\theta_0'); 
-ylabel('\theta_1');
+surf(theta0scale, theta1scale, J_valScale);
+xlabel('\theta_0 scaled'); 
+ylabel('\theta_1 scaled');
 zlabel('Cost');
-title('MSE Cost');
+title('scaled MSE Cost');
 
 % figure 2 subplot 2: convergence rate
 subplot(2,2,2);
-plot(0:1:itterations, [costGrad0 Jpast], 'b');
+plot(0:1:itterations, [costGrad0 Jpast], 'g');
+hold on
+plot(0:1:itterations, [costGradScale0 JpastScale], 'r');
 title('Gradient Descent Convergence');
 
 % figure 2 subplot 3: contour plot
 subplot(2,2,3);
-contour(theta0, theta1, J_vals, logspace(-8, 10, 20))
+contour(theta0scale, theta1scale, J_valScale, logspace(-8, 10, 20))
 hold on;
-plot(thetaGrad(1), thetaGrad(2), 'gx', 'MarkerSize', 20, 'LineWidth', 4); % grad descent theta values
+plot(thetaGradScale(1), thetaGradScale(2), 'rx', 'MarkerSize', 20, 'LineWidth', 4); % grad descent theta values
 xlabel('\theta_0'); 
 ylabel('\theta_1');
 title('MSE Cost Contours');
-
 
 
